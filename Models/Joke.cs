cea5edc616ebd5ea50cf198af0b381d93f46a151
greg <gregory.stocker2@gmail.com>
2023-05-12 21:16:04 -0700

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace jokesApp.Models
+    // joke is the central part of our application
+{
+    public class Joke
+    {
+        // this Id is needed to reference it with the auto generated stuff from our Joke Controller.
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        //constructor. For now leaving empty, for other parts of the code, it needs to be empty like this. 
+        public Joke()
+        {
+             
+        }
+    }
+}
